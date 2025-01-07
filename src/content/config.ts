9c33be4c409fc90dd1b77ba4d5c2e804d4265084
onurqnel <onur.onel2230@gmail.com>
2025-01-07 14:54:42 -0500

    Fixed Bugs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,29 @@
-import { defineCollection } from "astro:content"
-import { AuthorSchema, PostSchema } from "~schemas"
+import { defineCollection, z } from 'astro:content';
+
+const authors = defineCollection({
+    type: 'content',
+    schema: z.object({
+        name: z.string(),
+        portfolio: z.string().url().optional(),
+        avatar: z.string().url().optional(),
+    }),
+});
 
 const posts = defineCollection({
     type: 'content',
-    schema: PostSchema,
-})
-
-const authors = defineCollection({
-    type: 'data',
-    schema: AuthorSchema,
-})
+    schema: z.object({
+        title: z.string(),
+        author: z.string(),
+        tags: z.array(z.string()),
+        heroImage: z.string().url().optional(),
+        createdAt: z.string(),
+        updatedAt: z.string(),
+        relatedPosts: z.array(z.string()).optional(),
+        description: z.string(),
+    }),
+});
 
 export const collections = {
-    posts,
     authors,
-}
+    posts,
+};
